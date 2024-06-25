32a7fef7b459ea492dc617d859f45e524d873c44
vyshnavi2 <kvyshnavi222@gmail.com>
2024-06-25 18:57:29 +1000

    about

---
+++ src/app/pages/about.tsx
@@ -0,0 +1,18 @@
+// pages/about.tsx
+const About = () => {
+  return (
+    <div>
+      <main>
+        <h1>About Us</h1>
+
+        <section>
+          <h2>Our Mission</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit faucibus felis, vel pretium magna suscipit sed.</p>
+        </section>
+
+      </main>
+    </div>
+  );
+};
+
+export default About;
